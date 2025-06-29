36b8a9de2da7740c54394d02a25f7c9e28cf8e4b
ashish <ashis@LAPTOP-T9AP3587>
2025-06-29 22:27:02 +0530

    create utils asyncHandler , ApiError and ApiResponse for standeried Errors and Responses

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import {connectDB} from './db/dbConnect.js'
+import {app} from './app.js'
+
+app.on("error" , (error)=>{
+    console.log("Error : ",error);
+    throw error;
+    
+})
 
 
-
-
-connectDB();
+connectDB()
+.then((result) => {
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(` Server is running at port :${process.env.PORT}`);
+        
+    })
+}).catch((err) => {
+    console.log("MongoDB connection failed!!", err)
+});
 
 
 
